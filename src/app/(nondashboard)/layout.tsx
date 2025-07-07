cb626f4ba2da10326aff6c2fd7cc6f4669a3b33c
Michal <L0GI0@users.noreply.github.com>
2025-07-07 21:43:22 +0200

    feat: add clerk middleware

--- src/app/(nondashboard)/layout.tsx
+++ src/app/(nondashboard)/layout.tsx
@@ -1,14 +1,17 @@
 import NonDashboardNavbar from '@/components/NonDashboardNavbar';
 import Footer from '@/components/Footer';
+import { ClerkProvider } from '@clerk/nextjs';
 
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <div className="nondashboard-layout">
-      <NonDashboardNavbar />
-      <main className="nondashboard-layout__main">
-        {children}
-      </main>
-      <Footer/>
-    </div>
+    <ClerkProvider>
+      <div className="nondashboard-layout">
+        <NonDashboardNavbar />
+        <main className="nondashboard-layout__main">
+          {children}
+        </main>
+        <Footer/>
+      </div>
+    </ClerkProvider>
   );
 }
